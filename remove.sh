fadd92dfd5c9b856d9f39b02f5854e5a95b8183e
hnxsettings@1 <141293896+HenBz10Real@users.noreply.github.com>
2024-07-06 05:41:26 +0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -7,7 +7,7 @@ sleep 2 && echo
 echo ""
 echo "Please wait, remove in your device... "
 sleep 1 && echo
-cmd package bg-dexopt-job
+cmd package bg-dexopt-job > /dev/null 2>&1
 settings delete global device_idle_constants light_after_inactive_to
 sleep 1 && echo 
 echo "Succes, done remove in your device."
